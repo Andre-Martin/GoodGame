6d9f61f9b4a96f314281754ea965ce61f9adfed8
Andre Martin <r.andre@live.ca>
2024-03-21 21:25:29 -0700

    More bug fixes

--- src/components/GameListItem.tsx
+++ src/components/GameListItem.tsx
@@ -28,7 +28,7 @@ const GameListItem = ({
             </p>
             <p className="description-header__year">({year})</p>
           </div>
-          <div className="card-body">{clearText(description.slice(0, 222))}</div>
+          <div className="card-body">(description?.slice(0, 222))</div>
         </div>
       </td>
     </tr>
