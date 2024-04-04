4b1877ee0a2bfa0f589df6d8ce2eccc438a94336
erball <erbolmongol@gmail.com>
2024-04-04 14:12:40 -0700

    Deleted Dedicated chat page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,6 @@ import ROUTES from "./utils/ROUTES";
 const Home = lazy(() => import("./pages/Home"));
 const HotBGList = lazy(() => import("./pages/HotList"));
 const SearchBGList = lazy(() => import("./pages/SearchBGList"));
-const Chat = lazy(() => import("./pages/Chat"));
 const SingleGame = lazy(() => import("./pages/SingleGame"));
 const Page404 = lazy(() => import("./pages/Page404"));
 
@@ -25,7 +24,6 @@ function App() {
           <Header />
           <Routes>
             <Route path={ROUTES.home} element={<Home />} />
-            <Route path={ROUTES.chat} element={<Chat />} />
             <Route path={ROUTES.boardGameList} element={<HotBGList />} />
             <Route
               path={`${ROUTES.boardGameItem}:id`}
