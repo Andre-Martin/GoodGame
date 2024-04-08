da5672e3b449e4ef65d14b2cbe7158266b7afe48
erball <erbolmongol@gmail.com>
2024-04-08 13:27:58 -0700

    function for converting date into proper format added

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -15,7 +15,7 @@ export const getYoutubeImgByVideoID = (url: string) => {
 export const clearText = (text?: string): string => {
   if (!text) return "";
   const regex = /&#.*;&#.*;/g;
-  const result = text.replaceAll(regex, "");
+  const result = text.replace(regex, "");
   return result;
 };
 
@@ -31,14 +31,44 @@ export const concatIDs = (arr: number[]): string => {
   return arr.join(",");
 };
 
-export const formatIDsFromSearch = (arr: any): number[] => {
+interface objectWithId {
+  id: string;
+}
+
+export const formatIDsFromSearch = (arr: objectWithId[]): number[] => {
   const result = [];
   for (const item of arr) {
-    result.push(item.id);
+    result.push(+item.id);
   }
   return result;
 };
 
 export const getRandomID = (): number => {
   return Math.floor(Math.random() * 1000);
+};
+
+export const getTimeAgo = (date: Date): string => {
+  const currentDateTime = new Date();
+  const timeDifference = currentDateTime.getTime() - date.getTime();
+
+  const seconds = Math.floor(timeDifference / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(months / 12);
+
+  if (years >= 1) {
+    return `${years} year${years > 1 ? "s" : ""} ago`;
+  } else if (months >= 1) {
+    return `${months} month${months > 1 ? "s" : ""} ago`;
+  } else if (days >= 1) {
+    return `${days} day${days > 1 ? "s" : ""} ago`;
+  } else if (hours >= 1) {
+    return `${hours} hour${hours > 1 ? "s" : ""} ago`;
+  } else if (minutes >= 1) {
+    return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
+  } else {
+    return `${seconds} second${seconds > 1 ? "s" : ""} ago`;
+  }
 };
